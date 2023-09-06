e4d5e8e47346813cc0d7a530dc90a8b3f849e48a
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-06 14:48:29 +0200

    deleting unique from the username

--- models/User.model.js
+++ models/User.model.js
@@ -2,29 +2,28 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: false,
-      unique: true,
-      trim: true,
+    {
+        username: {
+            type: String,
+            required: false,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+    }
 );
 
 const User = model("User", userSchema);
