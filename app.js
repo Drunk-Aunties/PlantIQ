f90838da061b4fd07fa1f82674ee8e0a880649c5
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-07 11:26:37 +0200

    changing fetch to axios to deploy

--- app.js
+++ app.js
@@ -7,28 +7,12 @@ var cors = require("cors");
 
 const app = express();
 require("./config")(app);
-app.use(
-    cors({
-        origin: [
-            "http://127.0.0.1:3000",
-            "https://127.0.0.1:3000",
-            "http://localhost:8888",
-            "https://localhost:8888",
-        ],
-    })
-);
 
 const capitalize = require("./utils/capitalize");
 const projectName = "plant-iq";
 
 app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-
     if (req.session.currentUser) {
         res.locals.currentUser = req.session.currentUser;
     }
